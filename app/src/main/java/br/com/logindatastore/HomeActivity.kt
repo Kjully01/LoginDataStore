66964a9d1509901b2e398531e7fed55461c909e7
Karen Jully <karenjully.kr15@gmail.com>
2022-06-12 22:37:55 -0300

    botão de logOut

--- app/src/main/java/br/com/logindatastore/HomeActivity.kt
+++ app/src/main/java/br/com/logindatastore/HomeActivity.kt
@@ -1,8 +1,11 @@
 package br.com.logindatastore
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.datastore.preferences.core.booleanPreferencesKey
 import br.com.logindatastore.databinding.ActivityHomeBinding
+import kotlinx.coroutines.runBlocking
 
 class HomeActivity : AppCompatActivity() {
 
@@ -13,5 +16,22 @@ class HomeActivity : AppCompatActivity() {
 
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        listener()
+    }
+
+    private fun listener() {
+        binding.btnSignOut.setOnClickListener {
+            userLogOut()
+        }
+    }
+
+    private fun userLogOut() {
+        runBlocking {
+            DataStoreManager.setDataStore(preferencesKey = booleanPreferencesKey("HAS_LOGIN"), false)
+            val intent = Intent(this@HomeActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
